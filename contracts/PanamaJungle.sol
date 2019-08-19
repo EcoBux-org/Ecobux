ac86cf76fc929cb323d5839f626694e7477c4c89
Lucas Oberwager <lucas@ecobux.org>
2019-08-19 14:30:32 -0500

    Add Tabookey gassless and remove options to recieve ETH
    Also remove isContract()

--- contracts/PanamaJungle.sol
+++ contracts/PanamaJungle.sol
@@ -5,12 +5,13 @@ pragma experimental ABIEncoderV2;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 // Import OpenZeppelin's SafeMath Implementation
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "tabookey-gasless/contracts/RelayRecipient.sol";
 import "./utils/Ownable.sol";
 import "./utils/Pausable.sol";
 import "./utils/Erc20.sol";
 
 
-contract PanamaJungle is ERC721, Ownable, Pausable {
+contract PanamaJungle is ERC721, Ownable, Pausable, RelayRecipient {
 
     using SafeMath for uint256;
 
@@ -49,10 +50,10 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
     );
 
     // Event emitted when ECOB are transferred from user to contract
-    event EcoTransfer(
+    /*event EcoTransfer(
         address owner,
         uint256 amount
-    );
+    );*/
 
     // Event emitted when a new mircoAddon is created
     event NewAddon(
@@ -77,7 +78,7 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
 
     // Fallback function
     // solhint-disable-next-line no-empty-blocks
-    function() external payable {
+    function() external {
     }
 
     /** @dev Function to group create allotments
@@ -86,7 +87,7 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
         * (precision is not accuracy, note https://gis.stackexchange.com/a/8674 )
     * @return The new allotment's ID
     */
-    function createAllotment(uint256[2][][] calldata _allotments) external payable onlyOwner returns (bool success) {
+    function createAllotment(uint256[2][][2001] calldata _allotments) external onlyOwner returns (bool success) {
         uint16[] memory addons;
         for (uint i = 0; i < _allotments.length; i++) {
             Allotment memory newAllotment = Allotment({
@@ -108,7 +109,7 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
     /** @dev Function to create test allotments
     * @return The new allotment's ID
     */
-    function createTestAllotment() external payable onlyOwner returns (uint256) {
+    function createTestAllotment() external onlyOwner returns (uint256) {
         uint16[] memory addons;
         uint256[2][] memory geoPoints;
         Allotment memory newAllotment = Allotment({
@@ -129,14 +130,14 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
     /** @dev Function to buy one or more allotments
     * @param _tokensDesired number of tokens desired
     */
-    function buyAllotments(uint256 _tokensDesired, address _to) external payable whenNotPaused {
-        require(msg.sender != address(0) && msg.sender != address(this)); // Only be used by users to buy allotments
-        require(availableECO(msg.sender) >= currentPrice * _tokensDesired, "Not enough availalbe Ecobucks!");
+    function buyAllotments(uint256 _tokensDesired, address _to) external whenNotPaused {
+        require(get_sender() != address(0) && get_sender() != address(this)); // Only be used by users to buy allotments
+        require(availableECO(get_sender()) >= currentPrice * _tokensDesired, "Not enough availalbe Ecobucks!");
 
         // Take money from account before so no chance of re entry
-        takeEco(msg.sender, currentPrice * _tokensDesired);
+        takeEco(get_sender(), currentPrice * _tokensDesired);
 
-        uint256[] memory contractTokens = this.tokensOwnedByContract(); // Array of contract tokens for random selection
+        uint256[] memory contractTokens = this.ownedAllotments(address(this)); // Array of contract tokens for random selection
 
         require(contractTokens.length > _tokensDesired, "Not enough available tokens!"); // Need enough tokens available
 
@@ -146,7 +147,7 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
             nftAddress.safeTransferFrom(address(this), _to, tokenId); // Transfer token from contract to user
 
             emit Transferred(
-                msg.sender,
+                _to,
                 tokenId
             );
         }
@@ -177,13 +178,13 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
     * @param addonID Desired name of the addon mapped to an id
     * @return All microtransactions on tokenID
     */
-    function purchaseMicro(uint256 tokenID, uint16 addonID) external payable whenNotPaused returns (uint16[] memory) {
+    function purchaseMicro(uint256 tokenID, uint16 addonID) external whenNotPaused returns (uint16[] memory) {
         require(microAddons[addonID].purchasable, "Selected microaddon does not exist or is not purchasable.");
-        require(availableECO(msg.sender) > microAddons[addonID].price, "Not enough available Ecobucks!");
+        require(availableECO(get_sender()) > microAddons[addonID].price, "Not enough available Ecobucks!");
         require(_exists(tokenID), "Selected Token does not exist");
 
         // Take money from account
-        takeEco(msg.sender, microAddons[addonID].price);
+        takeEco(get_sender(), microAddons[addonID].price);
 
         allotments[tokenID].addons.push(addonID); // Add addonID to token array
 
@@ -200,8 +201,8 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
     /** @dev Function to get a list of owned allotment's IDs
     * @return A uint array which contains IDs of all owned allotments
     */
-    function ownedAllotments() external view returns(uint256[] memory) {
-        uint256 allotmentCount = balanceOf(msg.sender);
+    function ownedAllotments(address addy) external view returns(uint256[] memory) {
+        uint256 allotmentCount = balanceOf(addy);
         if (allotmentCount == 0) {
             return new uint256[](0);
         } else {
@@ -210,7 +211,7 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
             uint256 resultIndex = 0;
             uint256 allotmentId = 0;
             while (allotmentId < totalAllotments) {
-                if (ownerOf(allotmentId) == msg.sender) {
+                if (ownerOf(allotmentId) == addy) {
                     result[resultIndex] = allotmentId;
                     resultIndex = resultIndex.add(1);
                 }
@@ -219,36 +220,41 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
             return result;
         }
     }
-
-    /** @dev Function to get a list of owned allotment's IDs
-    * @return A uint array which contains IDs of all owned allotments
-    */
-    function tokensOwnedByContract() external view returns(uint256[] memory) {
-        uint256 allotmentCount = balanceOf(address(this));
-        if (allotmentCount == 0) {
-            return new uint256[](0);
-        } else {
-            uint256[] memory result = new uint256[](allotmentCount);
-            uint256 totalAllotments = allotments.length;
-            uint256 resultIndex = 0;
-            uint256 allotmentId = 0;
-            while (allotmentId < totalAllotments) {
-                if (ownerOf(allotmentId) == address(this)) {
-                    result[resultIndex] = allotmentId;
-                    resultIndex = resultIndex.add(1);
-                }
-                allotmentId = allotmentId.add(1);
-            }
-            return result;
-        }
-    }
-
+    
     /** @dev Function to retrieve a specific allotment's details.
     * @param id ID of the allotment who's details will be retrieved
     * @return Array id and geopoints of an allotment with all addons.
     */
     function allotmentDetails(uint256 id) external view returns (uint256, uint256[2][] memory, uint16[] memory) {
         return (id, allotments[id].geoMap, allotments[id].addons);
+    }
+
+    // Relay Functions to allow users to avoid needing a wallet
+    function accept_relayed_call(
+        address,    // Relay
+        address,    // From
+        bytes memory,      //encoded_function
+        uint,       // gas_price
+        uint        // transaction_fee
+    ) public view returns(uint32) {
+        // TODO: Only accept verified users
+        return 0; // accept everything.
+    }
+
+    // Relay Requires this func even if unused
+    function post_relayed_call(
+        address, //relay
+        address, //from
+        bytes memory, //encoded_function
+        bool, //success
+        uint, //used_gas
+        uint // transaction_fee
+    ) public {
+      // TODO: Add stuff here
+    }
+
+    function init_hub(RelayHub hubAddr) public onlyOwner {
+        init_relay_hub(hubAddr);
     }
 
     /** @dev Function to update _currentPrice
@@ -263,11 +269,10 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
       * @dev Throws if _ecoBucksAddress is not a contract address
       */
     function setEcoBucksAddress(address _ecoBucksAddress) public onlyOwner {
-        require(isContract(_ecoBucksAddress)); // ecoBucksAddress is common denominator contract for all subcontracts
         ecoBucksAddress = ERC20(_ecoBucksAddress);
     }
 
-    /** @dev Function to verifie user has enough ecobucks to spend
+    /** @dev Function to verify user has enough ecobucks to spend
     */
     function availableECO(address user) internal view returns (uint256) {
         return ecoBucksAddress.allowance(user, address(this));
@@ -278,26 +283,15 @@ contract PanamaJungle is ERC721, Ownable, Pausable {
     function takeEco(address _from, uint256 _amount) internal {
         require(availableECO(_from) > _amount); // Requre enough ecoBucks available
         require(ecoBucksAddress.transferFrom(_from, address(this), _amount), "Transfer of EcoBucks failed");
-        emit EcoTransfer(_from, _amount);
+        //emit EcoTransfer(_from, _amount);
     }
 
     /** @dev Function to create radnom numbers
       * @return psuedoRandom nnumbers
       */
     function random() internal returns(uint) {
-        uint randomNum = uint(keccak256(abi.encodePacked(now, msg.sender, randomNonce))) % 100;
+        uint randomNum = uint(keccak256(abi.encodePacked(now, get_sender(), randomNonce))) % 100;
         randomNonce++;
         return randomNum;
-    }
-
-    /** @dev Function determine if input is contract
-      * @return bool if input is a contract
-      */
-    function isContract(address _addr) internal view returns (bool) {
-        uint32 size;
-        assembly {
-            size := extcodesize(_addr)
-        }
-        return (size > 0);
     }
 }
--- migrations/3_deploy_panama.js
+++ migrations/3_deploy_panama.js
@@ -1,9 +1,28 @@
 const EcoBucks = artifacts.require("EcoBucks");
+const RelayHub = artifacts.require( "RelayHub");
 const PanamaJungle = artifacts.require("PanamaJungle");
 const PanamaFuture = artifacts.require("PanamaFuture");
 
-module.exports = function(deployer, network, accounts) {
-    console.log(EcoBucks.address)
-    deployer.deploy(PanamaJungle, EcoBucks.address);
+
+let networks = {
+  "ropsten-fork": {
+    relayHubAddr: "0x1349584869A1C7b8dc8AE0e93D8c15F5BB3B4B87"
+  },
+  "development": {
+    relayHubAddr: "0x9C57C0F1965D225951FE1B2618C92Eefd687654F"
+  }
+}
+
+module.exports = async (deployer, network, accounts) => {
     deployer.deploy(PanamaFuture, EcoBucks.address);
+
+    let hubAddr = networks[network].relayHubAddr
+    await deployer.deploy(PanamaJungle, EcoBucks.address)
+    let paj = await PanamaJungle.deployed();
+
+    let hub = await RelayHub.at(hubAddr).catch(e => {
+        console.log("error: ", e)
+    })
+    await hub.depositFor(PanamaJungle.address, { value:1e18 })
+    await paj.init_hub(hubAddr);
 };
