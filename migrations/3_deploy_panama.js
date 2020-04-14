ebbd7f6fa5db3c5ff4666b579f9eb5db91da5701
Watt3r <lucas@ecobux.org>
2020-04-14 00:11:18 -0700

    remove relayhub

--- migrations/3_deploy_panama.js
+++ migrations/3_deploy_panama.js
@@ -1,26 +1,10 @@
 const EcoBux = artifacts.require("EcoBux");
-const RelayHub = artifacts.require( "RelayHub");
 const PanamaJungle = artifacts.require("PanamaJungle");
 const PanamaFuture = artifacts.require("PanamaFuture");
-
-
-let networks = {
-  "ropsten-fork": {
-    relayHubAddr: "0x1349584869A1C7b8dc8AE0e93D8c15F5BB3B4B87"
-  },
-  "ropsten": {
-    relayHubAddr: "0x1349584869A1C7b8dc8AE0e93D8c15F5BB3B4B87"
-  },
-  "development": {
-    relayHubAddr: "0x9C57C0F1965D225951FE1B2618C92Eefd687654F"
-  }
-}
 
 module.exports = async (deployer, network, accounts) => {
     deployer.deploy(PanamaFuture, EcoBux.address);
 
-    console.log(network);
-    let hubAddr = networks[network].relayHubAddr
     await deployer.deploy(PanamaJungle, EcoBux.address)
     let paj = await PanamaJungle.deployed();
 
