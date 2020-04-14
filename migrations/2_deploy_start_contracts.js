1632f4301ab7b8fed4643b074a747a66dbff9d74
Watt3r <lucas@ecobux.org>
2020-04-14 16:03:06 -0700

    Fix race condition in migrations

--- migrations/2_deploy_start_contracts.js
+++ migrations/2_deploy_start_contracts.js
@@ -1,9 +1,14 @@
 const EcoBux = artifacts.require("EcoBux");
 const MarketPlace = artifacts.require("MarketPlace");
+const PanamaJungle = artifacts.require("PanamaJungle");
+const PanamaFuture = artifacts.require("PanamaFuture");
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(EcoBux).then(function() {
-        console.log(EcoBux.address)
-        return deployer.deploy(MarketPlace, EcoBux.address);
+    deployer.then(async () => {
+      await deployer.deploy(EcoBux);
+      await deployer.deploy(MarketPlace, EcoBux.address);
+      await deployer.deploy(PanamaFuture, EcoBux.address);
+      await deployer.deploy(PanamaJungle, EcoBux.address);
+
     });
 };
