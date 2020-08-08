0f12c50a1fc26a979e4476911b27727bdcb16649
Watt3r <lucas@ecobux.org>
2020-08-08 16:31:03 -0400

    Add migrations/ to prettier

--- migrations/2_deploy_start_contracts.js
+++ migrations/2_deploy_start_contracts.js
@@ -4,12 +4,12 @@ const MarketPlace = artifacts.require("MarketPlace");
 const PanamaJungle = artifacts.require("PanamaJungle");
 const PanamaFuture = artifacts.require("PanamaFuture");
 
-module.exports = function(deployer, network, accounts) {
-    deployer.then(async () => {
-      await deployer.deploy(EcoBux);
-      await deployer.deploy(EcoBuxFee);
-      await deployer.deploy(MarketPlace, EcoBux.address, EcoBuxFee.address);
-      await deployer.deploy(PanamaFuture, EcoBux.address);
-      await deployer.deploy(PanamaJungle, EcoBux.address);
-    });
+module.exports = function (deployer, network, accounts) {
+  deployer.then(async () => {
+    await deployer.deploy(EcoBux);
+    await deployer.deploy(EcoBuxFee);
+    await deployer.deploy(MarketPlace, EcoBux.address, EcoBuxFee.address);
+    await deployer.deploy(PanamaFuture, EcoBux.address);
+    await deployer.deploy(PanamaJungle, EcoBux.address);
+  });
 };
--- test-environment.config.js
+++ test-environment.config.js
@@ -4,8 +4,8 @@ module.exports = {
     ether: 100, // Initial balance of unlocked accounts (in ether)
   },
   setupProvider: (baseProvider) => {
-    const {GSNDevProvider} = require('@openzeppelin/gsn-provider');
-    const {accounts} = require('@openzeppelin/test-environment');
+    const {GSNDevProvider} = require("@openzeppelin/gsn-provider");
+    const {accounts} = require("@openzeppelin/test-environment");
     return new GSNDevProvider(baseProvider, {
       txfee: 70,
       useGSN: false,
